ad1ea39139f1f1b21cbd6d527096b993608c40aa
Alex Land <aland@sfu.ca>
2015-07-25 14:05:23 -0700

    Update permissions, add insert employee to db

--- app/src/main/java/com/sap/matcha/SettingsActivity.java
+++ app/src/main/java/com/sap/matcha/SettingsActivity.java
@@ -1,9 +1,13 @@
 package com.sap.matcha;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.annotation.NonNull;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,7 +37,20 @@ public class SettingsActivity extends PreferenceActivity {
 
     @Override
     public void onDestroy() {
+        MySQLiteHelper dbHelper = new MySQLiteHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
 
+        SharedPreferences settings = getSharedPreferences(MainActivity.SHARED_PREFERENCES,  MODE_PRIVATE);
+        String email = settings.getString(getString(R.string.user_email), "");
+        String name = settings.getString(getString(R.string.user_name), "");
+        String dept = settings.getString(getString(R.string.user_department), "");
+        String ama = settings.getString(getString(R.string.user_ama), "");
+        String location = settings.getString(getString(R.string.user_location), "");
+
+        TelephonyManager tMgr = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+        String phone = tMgr.getLine1Number();
+
+        dbHelper.insertEmployee(db, email, name, dept, ama, phone, location);
     }
 
 }
